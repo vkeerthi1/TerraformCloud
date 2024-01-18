412aefe788710fbc61245d195d0033c5287abd0e
keerthi v <91740419+vkeerthi1@users.noreply.github.com>
2024-01-18 13:13:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  cloud {
+    organization = "Terraform-keer"
+
+    workspaces {
+      name = "dec-cli"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0.2"
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "keerthiRG"
+  location = "westeurope"
+}
